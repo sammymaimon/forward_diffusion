5267cbfd4790b3ac01a382eadbe24bb3a312c8df
sammy <sammymaimon@gmail.com>
2023-03-09 10:44:18 +0200

    animation for spiral

---
+++ forward_diffusion/scripts/scatter_animation.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import glob
+
+
+frames = []
+num_pics = 17
+for i in range(num_pics):
+    new_frame = Image.open('../images/png/step'+str(i+2)+'.png')
+    frames.append(new_frame)
+frames[0].save('../png_to_gif.gif', format='GIF',
+               append_images=frames,
+               save_all=True,
+               duration=400, loop=0)
