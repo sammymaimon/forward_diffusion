f8e14ffc91983d3029a15b9c5304ec4c632a0a05
sammy <sammymaimon@gmail.com>
2023-03-09 16:11:46 +0200

    animation for spiral

---
+++ forward_diffusion/scripts/forward_steps.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from generate_distribution import dist
+
+def add_noise_and_print( mu, std,dist):
+    noisex = np.random.normal(mu, std, len(dist))
+    noisey = np.random.normal(mu, std, len(dist))
+    noisey_dist_x = dist[:, 0] * np.sqrt(1-std) + noisex
+    noisey_dist_y = dist[:, 1] * np.sqrt(1-std) + noisey
+    noise_1 = np.column_stack((noisey_dist_x, noisey_dist_y))
+    print('New', noise_1)
+
+    plt.scatter(noise_1[:, 0], noise_1[:, 1], alpha=0.1)
+    plt.xlim([-1.5, 1.5])
+    plt.ylim([-1.5, 1.5])
+    plt.show()
+
+    for i in range(2,20):
+        noisex = np.random.normal(mu, std, len(dist))
+        noisey = np.random.normal(mu, std, len(dist))
+        # noisey_dist_x += noisex
+        # noisey_dist_y += noisey
+        noisey_dist_x = noisey_dist_x * np.sqrt(1 - std) + noisex
+        noisey_dist_y = noisey_dist_y * np.sqrt(1 - std) + noisey
+        noise_1 = np.column_stack((noisey_dist_x, noisey_dist_y))
+        print('New', noise_1)
+
+        plt.scatter(noise_1[:, 0], noise_1[:, 1], alpha=0.1)
+        plt.xlim([-1.5, 1.5])
+        plt.ylim([-1.5, 1.5])
+        plt.show()
+        plt.savefig('../images/png/step' +str(i) + '.png')
+        plt.clf()
+        np.savetxt('../images/txt/step' + str(i) + '.txt', np.c_[noisey_dist_x,noisey_dist_y])
+
+
+
+mu = 0.0
+std = 0.05
+add_noise_and_print(mu, std, dist)
+
+print('old', dist)
