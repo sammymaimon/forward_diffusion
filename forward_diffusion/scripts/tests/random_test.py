8e74cc9504c026bcec236b8347ec6f8800695f90
sammymaimon <72603410+sammymaimon@users.noreply.github.com>
2023-05-01 15:30:20 +0300

    Add files via upload

--- forward_diffusion/scripts/tests/random_test.py
+++ forward_diffusion/scripts/tests/random_test.py
@@ -6,45 +6,44 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset, random_split
 
+
 N = 100
 mu = 0.02
 std = 2e-1
 noise = []
 
-#
-# X1_plot, X2_plot = np.meshgrid(np.linspace(-1., 1., N), np.linspace(-1., 1., N))
-# noise = (np.stack([X1_plot.reshape(N ** 2), X2_plot.reshape(N ** 2)])).T
 
+# X1_plot, X2_plot = np.meshgrid(np.linspace(-1., 1., N), np.linspace(-1., 1., N))
+# Z = torch.tensor(np.stack([X1_plot.reshape(N ** 2), X2_plot.reshape(N ** 2)]), dtype= torch.float32).T
 noise = np.array((np.random.normal(mu, std, N ** 2), np.random.normal(mu, std, N ** 2))).T
 noise_shifted = noise.copy()
-randvals = np.random.choice(len(noise), size=int(0.05 * len(noise)), replace=False)
 
-# print(randvals.shape)
-# exit()
+
 
 for i in range(len(noise)):
-    if i in randvals:
-        noise_shifted[i, 0] += np.random.normal(0, 0.1)
-    elif noise[i, 0] <= 0:
-        noise_shifted[i, 0] -= 0.1
+    if noise[i, 0] <= 0:
+        noise_shifted[i, 0] -= 0.1 + np.random.normal(0, 0.01)
     elif noise[i, 0] > 0:
-        noise_shifted[i, 0] += 0.1
+        noise_shifted[i, 0] += 0.1 + np.random.normal(0, 0.01)
 
-plt.scatter(noise[:,0],noise[:,1])
-plt.scatter(noise_shifted[:,0],noise_shifted[:,1])
-plt.show()
+
+# plt.scatter(noise[:,0],noise[:,1])
+# plt.scatter(noise_shifted[:,0],noise_shifted[:,1])
+# plt.show()
 
 
 X, y = noise, noise_shifted
-n_epochs = 125
+n_epochs = 150
 history = []
 y_pred_save = []
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, shuffle=True)
 X_train = torch.tensor(X_train, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.float32)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32)
+
 
 X_train = X_train.clone().detach()
 y_train = y_train.clone().detach()
@@ -78,7 +77,7 @@ for epoch in range(n_epochs):
         optimizer.step()
 
         model.eval()
-        y_pred = model(X_test)
+        y_pred = model(y_test)
         mse = loss_fn(y_pred, y_test)
         mse = float(mse)
         history.append(mse)
@@ -100,10 +99,13 @@ def arrow_plot(ax: plt.Axes, X, y, n=None, color='k'):
         ax.arrow(X[i, 0], X[i, 1], dx, dy, color=color, alpha=0.25)
 
 
-n_show = 500
+n_show = 400
 fig = plt.figure()
 ax = plt.gca()
 ax.plot(x_numpy[:n_show, 0], x_numpy[:n_show, 1], '.', color='k')
 arrow_plot(ax, x_numpy, y_numpy, n=n_show, color='r') # predicted values
 arrow_plot(ax, x_numpy, y_test, n=n_show, color='k') # expected values
 plt.show()
+
+
+a = y_numpy - y_test.detach().numpy()
